b481dce1b446e4f1b7603a2a339829ac3c7be85b
Rene Ballestreros Machado Jr <renebmjr@gmail.com>
2024-11-04 08:35:41 -0300

    Atualizacoes
    Atualizadas imagens da pista, da galinha e dos carros
    Implementada funcionalidade para o player 2 poder se movimentar
    Adicionadas funcionalidades para alinhar os carros nas vias das pistas

--- js/main.js
+++ js/main.js
@@ -5,8 +5,13 @@ const config = {
     physics: {
         default: 'arcade',
         arcade: {
-            debug: false,
+            debug: true,
         }
+    },
+    audio: {
+        disableWebAudio: true,
+        noAudio: false,
+        AudioContext: false
     },
     scene: {
         preload: preload,
@@ -17,49 +22,84 @@ const config = {
 
 const game = new Phaser.Game(config);
 
+let fps = 0;
+let lastTime = 0;
+
 function preload() {
     this.load.image('pista', 'assets/pista.png');
     this.load.spritesheet('galinha', 'assets/galinha.png', { frameWidth: 12, frameHeight: 8 });
+    this.load.spritesheet('carro', 'assets/car.png', { frameWidth: 16, frameHeight: 10 });
 }
 
 function create() {
+    this.time = this.game.time;
+
     // Adiciona a imagem de fundo da pista e ajusta a escala
     let pista = this.add.image(400, 300, 'pista');
     pista.setDisplaySize(800, 600); // Ajusta o tamanho da pista para preencher a tela
 
     // Adiciona o jogador 1
-    this.player1 = this.physics.add.sprite(205, 587, 'galinha', 1);
+    this.player1 = this.physics.add.sprite(205, 587, 'galinha');
     this.player1.setCollideWorldBounds(true);
     this.player1.scale = 3; // Ajuste a escala conforme necessário
 
 
     // Adiciona o jogador 2
-    this.player2 = this.physics.add.sprite(542, 587, 'galinha', 1);
+    this.player2 = this.physics.add.sprite(542, 587, 'galinha');
     this.player2.setCollideWorldBounds(true);
     this.player2.scale = 3; // Ajuste a escala conforme necessário
 
     // Controle do jogador
     this.cursors = this.input.keyboard.createCursorKeys();
 
-    // Adiciona carros
-    this.cars = this.physics.add.group({
-        key: 'galinha',
-        repeat: 5,
-        setXY: { x: 100, y: 100, stepX: 100 },
+    // Controle do jogador 2 (WASD)
+    this.keys = this.input.keyboard.addKeys({
+        w: Phaser.Input.Keyboard.KeyCodes.W,
+        a: Phaser.Input.Keyboard.KeyCodes.A,
+        s: Phaser.Input.Keyboard.KeyCodes.S,
+        d: Phaser.Input.Keyboard.KeyCodes.D,
     });
 
-    this.cars.children.iterate(function (car) {
-        car.setVelocityX(Phaser.Math.Between(100, 300));
-        car.setBounce(1);
-        car.setCollideWorldBounds(true);
+    // Cria uma array com 10 cores para cada carro
+    this.colors = [
+        0x9c2020, 0xb4e490, 0x646410, 0xe08888, 0x1c209c,
+        0x985c28, 0x6874d0, 0xb03c3c, 0x84b468, 0xd0d050
+    ]
+
+    // Adiciona carros
+    this.cars = this.physics.add.group({
+        key: 'carro',
+        repeat: 9,
+        setXY: { x: 100, y: 90, stepX: 150, stepY: 50 },
     });
+
+    // Ajusta a velocidade e a sensibilidade dos carros
+    this.cars.children.iterate(function (car, index) {
+        car.setVelocityX(Phaser.Math.Between(100, 300)); // Ajuste a velocidade aleatória dos carros
+        car.setBounce(1); // Ajuste a sensibilidade dos carros
+        car.scale = 2.3;
+        car.setCollideWorldBounds(true);
+        car.setTint(this.colors[index]); // Atribui uma cor única para cada carro
+    }, this);
 
     // Colisão entre jogador e carros
     this.physics.add.collider(this.player1, this.cars, hitCar, null, this);
     this.physics.add.collider(this.player2, this.cars, hitCar, null, this);
 }
 
-function update() {
+function update(time) {
+    if (!this.cursors) {
+        return;
+    }
+    
+    fps = 1000 / (time - lastTime);
+    lastTime = time;
+    // console.log(`FPS: ${fps}`);
+
+    if (fps < 30 || fps > 60) {
+        return;
+    }
+    
     // Controle do jogador 1
     if (this.cursors.left.isDown) {
         this.player1.setVelocityX(-160);
@@ -78,13 +118,35 @@ function update() {
     }
 
     // Controle do jogador 2 (adicionar teclas separadas, por exemplo, WASD)
-    // Exemplo básico:
-    // if (wasd.left.isDown) { ... }
+
+    if (this.keys.w.isDown) {
+        this.player2.setVelocityY(-160);
+    } else if (this.keys.s.isDown) {
+        this.player2.setVelocityY(160);
+    } else {
+        this.player2.setVelocityY(0);
+    }
+
+    if (this.keys.a.isDown) {
+        this.player2.setVelocityX(-160);
+    } else if (this.keys.d.isDown) {
+        this.player2.setVelocityX(160);
+    } else {
+        this.player2.setVelocityX(0);
+    }
 }
 
 function hitCar(player, car) {
     this.physics.pause();
     player.setTint(0xff0000);
-    player.anims.play('turn');
     this.gameOver = true;
 }
+
+// Para chamar a função update, você pode usar a seguinte lógica
+function loop(time) {
+    update(time);
+    requestAnimationFrame(loop);
+}
+
+// Inicie o loop
+loop();
